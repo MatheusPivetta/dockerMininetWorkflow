3cb4fee9ab9c0bc3391efbc0764a75a1cde22676
MatheusPivetta <matheusbp21@gmail.com>
2024-02-15 12:07:17 -0300

    fix

--- workflow-volume/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-service openvswitch-switch start
-echo "Iniciando"
-python3 topology.py
-echo "Finalizado"
-
-
---
+++ workflow-volume/entrypoint/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+service openvswitch-switch start
+echo "Instalando requisitos"
+apt-get update
+ln -fs /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime
+DEBIAN_FRONTEND=noninteractive apt-get install hping3 -y
+echo "Finalizando instalação de requisitos"
+echo "Iniciando topologia"
+python3 topology.py
+echo "Finalizado topologia"
